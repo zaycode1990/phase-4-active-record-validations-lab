163af4039609140cf7da0e80a055773e49074a11
Isaiah <ijandicos90@gmail.com>
2022-08-20 00:16:11 -0400

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+
+  validates :content, length: { minimum: 250 }
+
+  validates :summary, length: { maximum: 250 }
+
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+  validate :clickbait?
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i,
+  ]
+
+  def clickbait?
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
